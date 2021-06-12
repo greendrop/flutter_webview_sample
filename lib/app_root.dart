8141b76617812b26483546e3f570a1f6ae509858
greendrop <greendrop.devel@gmail.com>
2021-06-12 18:56:29 +0900

    add routemaster

--- lib/app_root.dart
+++ lib/app_root.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_webview_sample/components/pages/main_page.dart';
 import 'package:flutter_webview_sample/config/app_config.dart';
+import 'package:routemaster/routemaster.dart';
 
 class AppRoot extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final appConfig = AppConfig();
     if (!appConfig.displayEnv) {
-      return AppRootChild();
+      return AppRootMain();
     }
 
     return Directionality(
@@ -17,21 +18,24 @@ class AppRoot extends HookWidget {
           color: Colors.red,
           message: appConfig.env.toUpperCase(),
           location: BannerLocation.topStart,
-          child: AppRootChild()),
+          child: AppRootMain()),
     );
   }
 }
 
-class AppRootChild extends HookWidget {
+class AppRootMain extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WebView Sample',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MainPage(),
-    );
+    return MaterialApp.router(
+        title: 'WebView Sample',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routeInformationParser: const RoutemasterParser(),
+        routerDelegate: RoutemasterDelegate(
+            routesBuilder: (context) => RouteMap(routes: {
+                  '/': (_) => MaterialPage<Widget>(child: MainPage())
+                })));
   }
 }
